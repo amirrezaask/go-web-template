3bb09b4a63a0cfffeffec698b53ca137f29d6b42
amirrezaask <raskarpour@gmail.com>
2020-07-05 10:39:32 +0430

    improve redis

--- kv/redis.go
+++ kv/redis.go
@@ -12,27 +12,40 @@ func setRedisDefaultConfigs() {
 	config.C.Set("kv.redis.database", "0")
 	config.C.Set("kv.redis.ttl", 300)
 }
-func loadRedisConfigurationFromConfig() *redis.Options {
 
+func loadRedisConfigurationFromConfig() *redis.Options {
 	host := config.C.GetString("kv.redis.host")
 	port := config.C.GetString("kv.redis.port")
 	username := config.C.GetString("kv.redis.username")
 	password := config.C.GetString("kv.redis.password")
 	database := config.C.GetInt("kv.redis.database")
 	return &redis.Options{
-		Addr: fmt.Sprintf("%s:%s", host, port),
+		Addr:     fmt.Sprintf("%s:%s", host, port),
 		Username: username,
 		Password: password,
-		DB: database,
+		DB:       database,
 	}
 }
-func NewRedis() (*redis.Client, error) {
-	setRedisDefaultConfigs()
-	options := loadRedisConfigurationFromConfig()
-	c := redis.NewClient(options)
-	stat := c.Ping()
+
+var cli *redis.Client
+
+func connect(options *redis.Options) (*redis.Client, error) {
+	cli = redis.NewClient(options)
+	stat := cli.Ping()
 	if stat.Err() != nil {
 		return nil, stat.Err()
 	}
-	return c, nil
+	return cli, nil
+}
+
+func NewRedis() (*redis.Client, error) {
+	setRedisDefaultConfigs()
+	options := loadRedisConfigurationFromConfig()
+	if cli == nil {
+		return connect(options)
+	}
+	if err := cli.Ping().Err(); err != nil {
+		return connect(options)
+	}
+	return cli, nil
 }
