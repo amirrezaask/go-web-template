85bf1ed0b81ed0e3b1a628cd995e798ac5796b02
amirrezaask <raskarpour@gmail.com>
2020-07-05 22:19:17 +0430

    add docs for db/model.go

--- db/model.go
+++ db/model.go
@@ -1,13 +1,16 @@
 package db
 
+//Fillable is the interface that should be implementd by everyone that wants to use Scan.
 type Fillable interface {
 	Fields() []interface{}
 }
 
+//Scannable is implemented by sql.Rows and sql.Row so we can use them in Scan function.
 type Scannable interface {
 	Scan(dest ...interface{}) error
 }
 
+//Scan given scannable into the fillable pointers.
 func Scan(scannable Scannable, fillable Fillable) error {
 	return scannable.Scan(fillable.Fields()...)
 }
