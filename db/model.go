39407326986840362f4f0753167ba43e27255330
amirrezaask <raskarpour@gmail.com>
2020-07-06 10:28:39 +0430

    update DB model abstraction for simpler implementation

--- db/model.go
+++ db/model.go
@@ -1,16 +1,84 @@
+/*
+
+type User struct {
+	Id int
+	Friends Users
+}
+func (u *User) Scan(row Row) error {
+	return row.Scan(&u.Id)
+}
+func (u *User) LoadFriends(rows Rows) error {
+	return u.Friends.Scan(rows)
+}
+
+type Users []*User
+func (us Users) Scan(row Rows) error {
+	for row.Next() {
+		u := &User{}
+		err := row.Scan(&u.Id)
+		if err != nil {
+			return err
+		}
+		us = append(us, u)
+	}
+	return nil
+}
+
+func main() {
+	var db *sql.DB
+	users := Users{}
+	err := Query(db, users, `SELECT * FROM users`)
+	if err != nil {
+		panic(err)
+	}
+	user := &User{}
+	err = QueryRow(db, user, `SELECT * FROM users WHERE id=?`, 1)
+	friends := Users{}
+	err =  Query(db, friends, `SELECT * FROM users WHERE friend_id=?`, 1)
+}
+*/
+
 package db
 
-//Fillable is the interface that should be implementd by everyone that wants to use Scan.
-type Fillable interface {
-	Fields() []interface{}
-}
+import (
+	"database/sql"
+)
 
-//Scannable is implemented by sql.Rows and sql.Row so we can use them in Scan function.
-type Scannable interface {
+//Row is the composition of *sql.Row and *sql.Rows
+type Row interface {
 	Scan(dest ...interface{}) error
 }
+type Rows interface {
+	Row
+	Next() bool
+}
 
-//Scan given scannable into the fillable pointers.
-func Scan(scannable Scannable, fillable Fillable) error {
-	return scannable.Scan(fillable.Fields()...)
+//RowScanner should be implemented by all of the types who can get their data from Row.
+type RowScanner interface {
+	Scan(Row) error
+}
+
+type RowsScanner interface {
+	Scan(Rows) error
+}
+
+//Query a single row, use when you are sure that you're query selects only one row of data.
+func QueryRow(db *sql.DB, rs RowScanner, query string, params ...interface{}) error {
+	row := db.QueryRow(query, params...)
+	return rs.Scan(row)
+}
+
+//Query multiple rows.
+func Query(db *sql.DB, rs RowsScanner, query string, params ...interface{}) error {
+	rows, err := db.Query(query, params...)
+	if err != nil {
+		return err
+	}
+	for rows.Next() {
+		err = rs.Scan(rows)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
