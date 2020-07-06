b021895763fbf8460f0bb61e80854942bfd3c59a
amirrezaask <raskarpour@gmail.com>
2020-07-06 11:12:20 +0430

    add constructor for database provider

--- db/db.go
+++ db/db.go
@@ -1,8 +1,25 @@
 package db
 
 import (
+	"app/config"
 	"database/sql"
+	"fmt"
 )
 type Provider interface {
 	DB() (*sql.DB, error)
+}
+
+func NewProvider() (Provider, error) {
+	config.C.Set("database.type", "sqlite3")
+	dbType := config.C.GetString("database.type")
+	switch dbType {
+	case "mysql":
+		return &Mysql{}, nil
+	case "postgres":
+		return &Postgres{}, nil
+	case "sqlite3":
+		return &Sqlite{}, nil
+	default:
+		return Provider(nil), fmt.Errorf("%s is not supported as a database provider", dbType)
+	}
 }
