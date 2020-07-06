8bec66d7c270452e07f4143f1a5936857195b0d6
amirrezaask <raskarpour@gmail.com>
2020-07-06 10:57:49 +0430

    add sqlite3 db provider

--- db/sqlite.go
+++ db/sqlite.go
@@ -1,1 +1,38 @@
 package db
+
+import (
+	"app/config"
+	"database/sql"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+type Sqlite struct {
+	conn *sql.DB
+}
+
+func (s *Sqlite) DB() (*sql.DB, error) {
+	path := config.C.GetString("database.path")
+	if s.conn == nil {
+		conn, err := sqliteConnect(path)
+		if err != nil {
+			return nil, err
+		}
+		s.conn = conn
+		return s.conn, nil
+	}
+	if err := s.conn.Ping(); err != nil {
+		return nil, err
+	}
+	return s.conn, nil
+}
+
+func sqliteConnect(path string) (*sql.DB, error) {
+	db, err := sql.Open("sqlite3", path)
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
+}
--- go.mod
+++ go.mod
@@ -12,6 +12,7 @@ require (
 	github.com/jmoiron/sqlx v1.2.0
 	github.com/labstack/echo-contrib v0.9.0
 	github.com/labstack/echo/v4 v4.1.16
+	github.com/mattn/go-sqlite3 v1.14.0 // indirect
 	github.com/mitchellh/go-homedir v1.1.0
 	github.com/mitchellh/mapstructure v1.3.2 // indirect
 	github.com/pelletier/go-toml v1.8.0 // indirect
