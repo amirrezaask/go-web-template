0af842f7d1271b83c24d62ae68b86c39e2ef9b88
amirrezaask <raskarpour@gmail.com>
2020-07-23 14:21:07 +0430

    fix some issues

--- config/config.go
+++ config/config.go
@@ -1,17 +1,25 @@
 package config
 
 import (
+	"log"
+	"os"
+
 	"github.com/golobby/config"
-	"github.com/golobby/config/feeder"
 )
 
+//C is Instance of golobby config in use.
 var C *config.Config
 
-func Init() {
-	config.New(config.Options{
-		Feeder: feeder.Json{
-			"config.json",
-		},
-		Env:    "",
-	})
+var (
+	//APPEnv is a variable that shows which environment application is running in. (dev, prod, testing)
+	APPEnv = os.Getenv("APPENV")
+)
+
+//Init initializes the config package and loads config file.
+func Init(opts config.Options) {
+	c, err := config.New(opts)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	C = c
 }
