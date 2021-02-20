22e616695870b24d637683a0f3d4c6d1169fd2dc
amirrezaask <raskarpour@gmail.com>
2021-02-21 01:04:25 +0330

    add db abstraction

--- pkg/db/db.go
+++ pkg/db/db.go
@@ -1,7 +1,15 @@
 package db
 
-import "database/sql"
+import (
+	"context"
+	"database/sql"
+)
 
-func NewSQLDB(driver, connection string) (*sql.DB, error) {
+type SQLDB interface {
+	ExecContext(ctx context.Context, stmt string, args ...interface{}) (sql.Result, error)
+	QueryContext(ctx context.Context, query string, args ...interface{}) (*sql.Rows, error)
+}
+
+func NewSQLDB(driver, connection string) (SQLDB, error) {
 	return sql.Open(driver, connection)
 }
