0b0a1e520ab5c2f068d6f7928da713b372715a35
amirrezaask <raskarpour@gmail.com>
2021-02-21 21:07:17 +0330

    add nats to mb package

--- pkg/mb/mb.go
+++ pkg/mb/mb.go
@@ -1,10 +1,16 @@
 package mb
 
+import "fmt"
+
 type MessageBroker interface {
-	Subscribe(topic string, handler func(message []byte) error) error
-	Publish(message []byte) error
+	Subscribe(topic string, handler func(message []byte)) error
+	Publish(topic string, message []byte) error
 }
 
-func NewMessageBroker() (MessageBroker, error) {
-	return nil, nil
+func NewMessageBroker(backend string, opts interface{}) (MessageBroker, error) {
+	switch backend {
+	case "nats":
+		return newNats(opts.(*NatsMbConfig))
+	}
+	return nil, fmt.Errorf("no backend matched for %v", backend)
 }
---
+++ pkg/mb/nats.go
@@ -0,0 +1,33 @@
+package mb
+
+import "github.com/nats-io/nats.go"
+
+type natsMb struct {
+	Conn *nats.Conn
+}
+
+func (n *natsMb) Subscribe(topic string, handler func(message []byte)) error {
+	_, err := n.Conn.Subscribe(topic, func(m *nats.Msg) {
+		handler(m.Data)
+	})
+	if err != nil {
+		return err
+	}
+	return nil
+}
+func (n *natsMb) Publish(topic string, message []byte) error {
+	return n.Conn.Publish(topic, message)
+}
+
+type NatsMbConfig struct {
+	Url     string
+	Options []nats.Option
+}
+
+func newNats(c *NatsMbConfig) (MessageBroker, error) {
+	conn, err := nats.Connect(c.Url, c.Options...)
+	if err != nil {
+		return nil, err
+	}
+	return &natsMb{conn}, nil
+}
